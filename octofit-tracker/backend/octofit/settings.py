8b6cb1af5b5d2d5c40282985c60c0b429e212936
apsarkather-ux <apsarkather@gmail.com>
2026-02-24 11:31:53 +0000

    settings.py

--- octofit-tracker/backend/octofit/settings.py
+++ octofit-tracker/backend/octofit/settings.py
@@ -73,8 +73,8 @@ WSGI_APPLICATION = 'octofit.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mongodb',
-        'NAME': 'octofit_db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
